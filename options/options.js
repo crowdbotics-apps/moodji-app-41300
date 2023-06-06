189b76d43b1254dda3a294ff9f3f6b60041f56a9
Crowdbotics <team@crowdbotics.com>
2023-06-06 17:06:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "moodji-app-41300",
+  url: "https://moodji-app-41300.botics.co",
+  api: "https://moodji-app-41300.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
